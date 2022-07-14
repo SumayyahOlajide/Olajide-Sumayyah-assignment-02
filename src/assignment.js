c80020aa0c2bfd3ab683bbe62429f86de185d1e5
Sumayyah Olajide <yemixs1423@gmail.com>
2022-07-14 16:40:06 +0000

    Challenge 1 and Challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,17 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  
+  let sum = 0;
+  
+  for(let i = 0; i < arrayOfNumbers.length; i++){
+    
+    sum += arrayOfNumbers[i];
+  }
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +36,19 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let evenNumberCount = 0;
+
+  for(let i = 0;i < arrayOfNumbers.length;i++){
+    let evenNumber = arrayOfNumbers[i] % 2 === 0;
+
+    if(evenNumber){
+      evenNumberCount++;
+    }
+  }
+    return evenNumberCount;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
